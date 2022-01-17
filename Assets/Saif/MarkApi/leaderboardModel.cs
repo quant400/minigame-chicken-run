c9caa0f97dcc4a1b794439c107c44fc271a6b14e
SouheilHous <souhailhous@gmail.com>
2022-01-18 02:53:14 +0900

    commit nft getter

--- Assets/Saif/MarkApi/leaderboardModel.cs
+++ Assets/Saif/MarkApi/leaderboardModel.cs
@@ -12,40 +12,27 @@ namespace leaderboardModel
 
 
 
+    
+        [System.Serializable]
+        public class allTimeLEaderBoard
+        {
+
+            public List<assetClass> allTimeLeadboeardClass;
+        }
     [System.Serializable]
     public class assetClass
     {
-        [JsonProperty("_id")]
-        public string Id { get; set; }
-
-        [JsonProperty("id")]
-        public long TemperatureId { get; set; }
-
-        [JsonProperty("dailyScore")]
-        public long DailyScore { get; set; }
-
-        [JsonProperty("allTimeScore")]
-        public long AllTimeScore { get; set; }
-
-        [JsonProperty("dailySessionPlayed")]
-        public long DailySessionPlayed { get; set; }
-
-        [JsonProperty("totalSessionPlayed")]
-        public long TotalSessionPlayed { get; set; }
-
-        [JsonProperty("updatedAt")]
-        public DateTimeOffset UpdatedAt { get; set; }
-
-        [JsonProperty("__v")]
-        public long V { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
+        public string _id;
+        public int id;
+        public int dailyScore;
+        public int allTimeScore;
+        public int dailySessionPlayed;
+        public int totalSessionPlayed;
+        public DateTime updatedAt;
+        public int __v;
+        public string name;
     }
-    [System.Serializable]
-    public class allTimeLEaderBoard
-    {
-        public List<assetClass> allTimeLeadboeardClass;
-    }
+
 }
+
 
---
+++ Assets/Saif/MarkApi/restApiDataView.cs
@@ -0,0 +1,208 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DataApi;
+using UnityEngine.Networking;
+using Tamarin.Common;
+using Tamarin.FirebaseX;
+using Newtonsoft.Json;
+
+public class restApiDataView : MonoBehaviour
+{
+    public static restApiDataView _instance;
+    public GameObject loadingIconPrefab;
+    GameObject _spawnedLoadingIcon;
+    public List<leaderboardModel.assetClass> leaderboadData;
+    public leaderboardModel.assetClass[] leaderboardArray;
+    public List<leaderboardModel.assetClass> dailyLeaderboadData;
+    public leaderboardModel.assetClass[] dailyLeaderboardArray;
+    FirebaseAPI firebase;
+
+    private void Awake()
+    {
+        _instance = this;
+        leaderboadData = new List<leaderboardModel.assetClass>();
+    }
+    private async void Start()
+    {
+        //here we are referencing the api, to make a shorthand for firebase. (cause we are lazy devs, and Firebase.Instance is too long to write every time :))
+        await Waiter.Until(() => FirebaseAPI.Instance.ready == true);
+        firebase = FirebaseAPI.Instance;
+    }
+    // Start is called before the first frame update
+    //callable function versions of the plain request for the leaderboard
+    public async void DisplayLeaderboard()
+    {
+        getLeaderboardFronRestApi();
+        _spawnedLoadingIcon = Instantiate(loadingIconPrefab, GameObject.Find("Canvas").transform);
+        var query = new Dictionary<string, object>();
+        query.Add("NumResults", 10);
+        leaderboardObject _Leaderboard = await firebase.functions.HttpsCall<leaderboardObject>("getLeaderboard", query);
+        Destroy(_spawnedLoadingIcon);
+        UIManager._instance.SpawnLeaderboard(_Leaderboard, "LEADERBOARD");
+
+    }
+    public async void DisplayDailyLeaderboard()
+    {
+        _spawnedLoadingIcon = Instantiate(loadingIconPrefab, GameObject.Find("Canvas").transform);
+        var query = new Dictionary<string, object>();
+        query.Add("NumResults", 10);
+        leaderboardObject _Leaderboard = await firebase.functions.HttpsCall<leaderboardObject>("getDailyLeaderboard", query);
+        Destroy(_spawnedLoadingIcon);
+        UIManager._instance.SpawnLeaderboard(_Leaderboard, "Daily LEADERBOARD");
+    }
+    public void getLeaderboardFronRestApi()
+    {
+        StartCoroutine(getLeaderboardFromApi("https://api.cryptofightclub.io/game/sdk/chicken/leaderboard/daily", "all"));
+    }
+    public void getDailyLeaderboardFronRestApi()
+    {
+        StartCoroutine(getLeaderboardFromApi("https://api.cryptofightclub.io/game/sdk/chicken/leaderboard/daily","daily"));
+    }
+    public IEnumerator getLeaderboardFromApi(string url,string type)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url.ToString()))
+        {
+            request.method = UnityWebRequest.kHttpVerbGET;
+            request.SetRequestHeader("Accept", "application/json");
+            yield return request.SendWebRequest();
+            if (request.error == null)
+            {
+                if (type == "daily")
+                {
+                    checkLeadboardDaily(request.downloadHandler.text);
+
+                }
+                else
+                {
+                    checkLeadboardAllTime(request.downloadHandler.text);
+
+                }
+                Debug.Log(request.downloadHandler.text);
+
+            }
+            else
+            {
+                Debug.Log("error in server");
+            }
+        }
+
+
+
+
+
+    }
+    public void checkLeadboardAllTime(string url)
+    {
+
+        string MatchData = fixJsonName(url);
+        Debug.Log(MatchData);
+        leaderboardArray = JsonUtil.fromJson<leaderboardModel.assetClass[]>(url);
+        if (leaderboardArray != null)
+        {
+            if (leaderboardArray.Length > 0)
+            {
+                for (int i = 0; i < leaderboardArray.Length; i++)
+                {
+                    leaderboadData.Add(leaderboardArray[i]);
+                }
+            }
+        }
+
+
+    }
+    public void checkLeadboardDaily(string url)
+    {
+
+        string MatchData = fixJsonName(url);
+        Debug.Log(MatchData);
+        dailyLeaderboardArray = JsonUtil.fromJson<leaderboardModel.assetClass[]>(url);
+        if (leaderboardArray != null)
+        {
+            if (dailyLeaderboardArray.Length > 0)
+            {
+                for (int i = 0; i < dailyLeaderboardArray.Length; i++)
+                {
+                    dailyLeaderboadData.Add(dailyLeaderboardArray[i]);
+                }
+            }
+        }
+
+
+    }
+    public void setDataOnLeadModel(List<leaderboardModel.assetClass> mainModel, leaderboardObject localObject)
+    {
+        if (mainModel != null)
+        {
+            if (mainModel.Count > 0)
+            {
+                for (int i = 0;i < mainModel.Count; i++)
+                {
+
+                }
+            }
+        }
+        
+    }
+    public LeaderboardUser (leaderboardModel.assetClass mainModel)
+    {
+        new LeaderboardUser user = new LeaderboardUser();
+    }
+    string fixJsonName(string value)
+    {
+        value = value.Substring(0, value.Length - 1);
+        value = value.Remove(0, 1);
+
+        return value;
+    }
+    string fixJson(string value)
+    {
+        value = "{\"Items\":" + value + "}";
+        return value;
+    }
+    public static class JsonUtil
+    {
+
+        /// <summary> Converts an object to a Json string </summary>.
+        /// <param name="obj">object </param>
+        public static string toJson<T>(T obj)
+        {
+            if (obj == null) return "null";
+
+            if (typeof(T).GetInterface("IList") != null)
+            {
+                Pack<T> pack = new Pack<T>();
+                pack.data = obj;
+                string json = JsonUtility.ToJson(pack);
+                return json.Substring(8, json.Length - 9);
+            }
+
+            return JsonUtility.ToJson(obj);
+        }
+
+        /// < summary > parse Json </summary >
+        /// <typeparam name="T">type</typeparam>
+        /// <param name="json">Json string </param>
+        public static T fromJson<T>(string json)
+        {
+            if (json == "null" && typeof(T).IsClass) return default(T);
+
+            if (typeof(T).GetInterface("IList") != null)
+            {
+                json = "{\"data\":{data}}".Replace("{data}", json);
+                Pack<T> Pack = JsonUtility.FromJson<Pack<T>>(json);
+                return Pack.data;
+            }
+
+            return JsonUtility.FromJson<T>(json);
+        }
+
+        /// < summary > inner packaging class </summary >
+        private class Pack<T>
+        {
+            public T data;
+        }
+
+    }
+
+}

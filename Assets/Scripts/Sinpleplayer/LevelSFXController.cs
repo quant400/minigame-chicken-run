c7874de02b517ceec97a473a8880c7c79422a6cc
Hassan-iqbal6 <43201426+Hassan-iqbal6@users.noreply.github.com>
2022-02-03 14:34:30 +0500

    added player prefs

--- Assets/Scripts/Sinpleplayer/LevelSFXController.cs
+++ Assets/Scripts/Sinpleplayer/LevelSFXController.cs
@@ -15,6 +15,22 @@ public class LevelSFXController : MonoBehaviour
         music = transform.GetChild(1).GetComponent<AudioSource>();
         defaultMusicVol = music.volume;
         defaultSFXVol = SFX.volume;
+
+        if(PlayerPrefs.HasKey("Music"))
+        {
+            if (PlayerPrefs.GetString("Music") == "off")
+            {
+                MuteMusic();
+            }
+        }
+
+        if (PlayerPrefs.HasKey("SFX"))
+            {
+            if (PlayerPrefs.GetString("SFX") == "off")
+            {
+                MuteSFX();
+            }
+        }
     }
 
 
@@ -23,10 +39,12 @@ public class LevelSFXController : MonoBehaviour
         if (gameplayView.instance.GetSFXMuted())
         {
             SFX.volume = defaultMusicVol;
+            PlayerPrefs.SetString("SFX", "on");
         }
         else
         {
             SFX.volume = 0;
+            PlayerPrefs.SetString("SFX", "off");
         }
 
         gameplayView.instance.SetSFXMuted(!gameplayView.instance.GetSFXMuted());
@@ -37,10 +55,12 @@ public class LevelSFXController : MonoBehaviour
         if(music.volume==0)
         {
             music.volume = defaultMusicVol;
+            PlayerPrefs.SetString("Music", "on");
         }
         else
         {
             music.volume = 0;
+            PlayerPrefs.SetString("Music", "off");
         }
     }
 }

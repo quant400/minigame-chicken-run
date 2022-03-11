69d3b1c7047db9a98fefd223d35b3f1867b5ab9b
Hassan-iqbal6 <43201426+Hassan-iqbal6@users.noreply.github.com>
2022-03-11 14:20:38 +0500

    improved fps counter

--- Assets/Scripts/Sinpleplayer/FPScounter.cs
+++ Assets/Scripts/Sinpleplayer/FPScounter.cs
@@ -6,12 +6,22 @@ using TMPro;
 public class FPScounter : MonoBehaviour
 {
     [SerializeField]
-    TMP_Text counter;
-   
+    TMP_Text counter,averageFps,minFps;
+    float min = Mathf.Infinity;
+    int framesPassed;
+    float totalFps;
     void Update()
     {
         float fps = 1 / Time.unscaledDeltaTime;
-        counter.text = "" + (int)fps;
+        counter.text = "FPS:" + (int)fps;
+        totalFps += fps;
+        framesPassed++;
+        averageFps.text = "Avg:" + (int)(totalFps / framesPassed);
+        if (fps < min && framesPassed > 10)
+        {
+            min = fps;
+            minFps.text = "Min: " + (int)min;
+        }
     }
 
 }
